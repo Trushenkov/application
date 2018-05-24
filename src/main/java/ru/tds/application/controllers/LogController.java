3b53c388d7cef90db7ad0f0b368d716584f5ef13
aivanov <na131@mail.ru>
2018-05-24 13:44:29 +0300

    Created

---
+++ src/main/java/ru/tds/application/controllers/LogController.java
@@ -0,0 +1,35 @@
+package ru.tds.application.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.tds.application.repositories.LogRepository;
+
+@RestController
+public class LogController {
+
+    @Autowired
+    private LogRepository repository;
+
+    /**
+     * Метод для выбора всех записей из таблицы log
+     *
+     * @return все записи из таблицы log
+     */
+    @RequestMapping(value = "log/all")
+    public String logAll(){
+        return repository.findAll().toString();
+    }
+
+    /**
+     * Метод для выбора из таблицы log записи, в которой значение id=1
+     *
+     * @return запись со значением id=1
+     */
+    @RequestMapping(value = "log/1")
+    public String log1(){
+        return repository.findById(1).toString();
+    }
+
+
+}
---
+++ src/main/java/ru/tds/application/controllers/RoleController.java
@@ -0,0 +1,68 @@
+package ru.tds.application.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ru.tds.application.domain.RolesEntity;
+import ru.tds.application.repositories.RolesRepository;
+
+@RestController
+public class RoleController {
+
+    @Autowired
+    private RolesRepository repository;
+
+    /**
+     * Метод для сохранения новой роли в таблицу roles
+     *
+     * @return новый пользователь в базе данных
+     */
+    @RequestMapping(value = "/role/save/super_admin", method = RequestMethod.GET)
+    public RolesEntity saveUser() {
+        return repository.save(
+                new RolesEntity(3,"SUPER_ADMIN", "This id role for SUPER ADMIN"));
+    }
+
+    /**
+     * Метод для выбора из таблицы роли со значением id=1
+     *
+     * @return роль со значением id=1
+     */
+    @RequestMapping(value = "/role/1")
+    public String role1(){
+        return repository.findById(1).toString();
+    }
+
+    /**
+     * Метод для выбора из таблицы роли со значением id=2
+     *
+     * @return роль со значением id=2
+     */
+    @RequestMapping(value = "/role/2")
+    public String role2(){
+        return repository.findById(2).toString();
+    }
+
+    /**
+     * Метод для выбора из таблицы роли со значением id=3
+     *
+     * @return роль со значением id=3
+     */
+    @RequestMapping(value = "/role/3")
+    public String role3(){
+        return repository.findById(3).toString();
+    }
+
+    /**
+     * Метод для выбора всех записей из таблицы roles
+     *
+     * @return все записи из таблицы
+     */
+    @RequestMapping(value = "/role/all")
+    public String roleAll(){
+        return repository.findAll().toString();
+    }
+
+
+}
