5375be54cdc784cdee1a6e901cc10b65879bbdcf
aivanov <na131@mail.ru>
2018-06-05 12:52:46 +0300

    add comments

--- src/main/java/ru/tds/application/Application.java
+++ src/main/java/ru/tds/application/Application.java
@@ -1,7 +1,6 @@
 package ru.tds.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,30 +10,41 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import ru.tds.application.domain.UsersEntity;
+import ru.tds.application.repositories.UsersRepository;
 
+/**
+ * Класс, в котором реализован запуск приложения и предоставление права ADMIN пользователю с id=3 из таблицы USERS
+ *
+ * @author Трушенков Дмитрий
+ */
 @EnableWebSecurity
 @SpringBootApplication
 @ComponentScan
 public class Application extends WebSecurityConfigurerAdapter {
 
+    private final UsersRepository repository;
+
+    @Autowired
+    public Application(UsersRepository repository) {
+        this.repository = repository;
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        UsersEntity user = repository.getById(3);
         auth
                 .inMemoryAuthentication()
-                .withUser(User.withDefaultPasswordEncoder().username("admin").password("admin1").roles("ADMIN"));
-//                .and()
-//                .inMemoryAuthentication().withUser(User.withDefaultPasswordEncoder().username("user").password("user").roles("USER"));
+                .withUser(User.withDefaultPasswordEncoder().username(user.getLogin()).password(user.getPassword()).roles("ADMIN"));
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+
         http.headers().addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"));
 
         http.authorizeRequests().antMatchers("/user/**").hasRole("ADMIN");
-
-//        http.authorizeRequests().antMatchers("/role/**").hasRole("ADMIN");
-
 
         http.httpBasic();
     }
