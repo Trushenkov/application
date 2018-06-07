8b1023712eb74fae4d1ddfc40999d0ae2f120a6d
aivanov <na131@mail.ru>
2018-06-07 14:29:57 +0300

    Updated user logging using ApplicationListener interface

--- src/main/java/ru/tds/application/aspect/LogSecurityAspect.java
+++ src/main/java/ru/tds/application/aspect/LogSecurityAspect.java
@@ -3,6 +3,9 @@ package ru.tds.application.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import ru.tds.application.domain.LogEntity;
 import ru.tds.application.domain.UsersEntity;
@@ -10,6 +13,7 @@ import ru.tds.application.repositories.LogRepository;
 import ru.tds.application.repositories.UsersRepository;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 /**
  * Класс,в котором реализована запись информации о пользователе в таблицу log, когда пользователь выполняет запрос /user/all
@@ -18,35 +22,51 @@ import java.sql.Date;
  */
 @Component
 @Aspect
-public class LogSecurityAspect {
+public class LogSecurityAspect implements ApplicationListener<AuthenticationSuccessEvent> {
 
-    private final UsersRepository userRepository;
+//    private final UsersRepository userRepository;
 
     private final LogRepository repository;
 
     @Autowired
-    public LogSecurityAspect(UsersRepository userRepository, LogRepository repository) {
-        this.userRepository = userRepository;
+    public LogSecurityAspect(LogRepository repository) {
         this.repository = repository;
     }
 
-    @Around("execution(* ru.tds.application.controllers.UsersController.findAll(..))")
-    public Object writeLog(ProceedingJoinPoint pjp) throws Throwable {
+//    @Around("execution(* ru.tds.application.controllers.UsersController.findAll(..))")
+//    public Object writeLog(ProceedingJoinPoint pjp) throws Throwable {
+//
+//        UsersEntity user = userRepository.getById(3);
+//        LogEntity log = new LogEntity();
+//
+//        java.util.Date today = new java.util.Date();//Текущая дата
+//        System.out.println(new java.sql.Time(today.getTime())); //Текущее время
+//
+//        //Запись в таблицу log новой строки, содержащей данные о пользователе
+//        log.setId(1);
+//        log.setUserLogin(user.getLogin());
+//        log.setDateLogin(new Date(today.getTime()));
+//        log.setDateLogout(new Date(118, 5, 5));
+//
+//        repository.save(log);
+//
+//        return pjp.proceed();
+//    }
 
-        UsersEntity user = userRepository.getById(3);
+    @Override
+    public void onApplicationEvent(AuthenticationSuccessEvent event) {
+        User userName = (User) event.getAuthentication().getPrincipal();
         LogEntity log = new LogEntity();
+        try {
+            java.util.Date today = new java.util.Date();//Текущая дата
+            log.setId(1);
+            log.setDateLogin(new Date(today.getTime()));
+            log.setUserLogin(userName.getUsername());
+            log.setDateLogout(new Date(118, 5, 8));
+            repository.saveAndFlush(log);
+        } catch (Exception ignored) {
+        }
 
-        java.util.Date today = new java.util.Date();//Текущая дата
-        System.out.println(new java.sql.Time(today.getTime())); //Текущее время
 
-        //Запись в таблицу log новой строки, содержащей данные о пользователе
-        log.setId(1);
-        log.setUserLogin(user.getLogin());
-        log.setDateLogin(new Date(today.getTime()));
-        log.setDateLogout(new Date(118, 5, 5));
-
-        repository.save(log);
-
-        return pjp.proceed();
     }
 }
